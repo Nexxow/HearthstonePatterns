25e3b2af8148c0554a38952d833dec87f1b5234f
crowlas <22192006+crowlas@users.noreply.github.com>
2018-01-12 15:20:18 +0100

    Plateau stock des serviteurs

--- HearthStone/src/plateau/Plateau.java
+++ HearthStone/src/plateau/Plateau.java
@@ -1,11 +1,42 @@
 package plateau;
 
+import java.util.ArrayList;
+
+import carte.Serviteur;
+import partie.Joueur;
+
 public class Plateau implements Observer {
+	private ArrayList<Serviteur> serviteursJ1;
+	private ArrayList<Serviteur> serviteursJ2;
+	private Joueur j1;
+	private Joueur j2;
+	
+	
+	public void monstreAttaqueMonstre(Serviteur c1, Serviteur c2){
+			c2.ajoutDefense(c1.getAttaque());
+			c1.ajoutDefense(c2.getAttaque());
+	}
+	
+	public void sortAttaqueMonstre(int degat, Serviteur c2){
+		c2.ajoutDefense(-degat);
+	}
+	
+	public void SoinMonstre(int degat, Serviteur c2){
+		this.sortAttaqueMonstre(-degat, c2);
+	}
 
 	@Override
 	public void actualiser() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public ArrayList<Serviteur> getServiteursJ1() {
+		return this.serviteursJ1;
+	}
+
+	public ArrayList<Serviteur> getServiteursJ2() {
+		return this.serviteursJ2;
 	}
 
 }
