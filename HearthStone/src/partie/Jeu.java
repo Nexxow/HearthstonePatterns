dc0bf647d7ee9ab349d1791f6e2baa1e684c3a50
crowlas <dracosamu@gmail.com>
2018-01-12 10:49:34 +0100

    Tour de jeu organisé

--- HearthStone/src/partie/Jeu.java
+++ HearthStone/src/partie/Jeu.java
@@ -17,27 +17,82 @@ public class Jeu {
 	}
 
 	/**
-	 * méthode lancé pour le tour d'un joueur
-	 * elle est lancé une fois par joueur à chaque tour
+	 * méthode lancé pour faire fonctionner la partie
+	 * elle s'arrête à la fin du jeu
+	 * @return 
 	 */
-	public void tourDeJeu(int tour){
-			int mana = getManaTour(tour);
-			Joueur joueurPerdant = this.j1.jouer(mana);
-			if (joueurPerdant!=null) this.finDuJeu(joueurPerdant);
-			joueurPerdant = this.j2.jouer(mana);
-			if (joueurPerdant!=null) this.finDuJeu(joueurPerdant);
-			
+	public Joueur lancerPartie(){
+		while (true) {			
+			int mana = getManaTour();
+			String joueur;
+			for (int i = 0; i <= 2; i++) {
+				if (i==1) joueur = "j1";
+				else joueur= "j2";
+				
+				Joueur joueurPerdant = this.jouer(joueur, mana);
+				if (joueurPerdant!=null) return this.finDuJeu(joueurPerdant);
+			}
+			this.tour+=1;
 		}
-		
 	}
 
-	private int getManaTour(int tour2) {
-		// TODO Auto-generated method stub
-		return 0;
+	/**
+	 * 	
+	 * @param joueur le joueur en train de jouer
+	 * @param mana max du joueur pour ce tour
+	 * @return le joueur perdant. Null si il n'y en a pas.
+	 */
+	private Joueur jouer(String joueur, int mana) {
+		Joueur joueurPerdant;
+		if (joueur=="j1"){
+			this.piocher(this.j1);
+			joueurPerdant = this.j1.jouer(mana);
+		}else{
+			this.piocher(this.j2);
+			joueurPerdant = this.j2.jouer(mana);
+		}
+		return joueurPerdant;
 	}
 
-	private void tour0() {
-		// TODO Auto-generated method stub
+	/**
+	 * Méthode lancé à la fin du jeu
+	 * @param joueurPerdant
+	 * @return
+	 */
+	private Joueur finDuJeu(Joueur joueurPerdant) {
+		return joueurPerdant;		
+	}
+
+	/**
+	 * 
+	 * @return la mana max. pour ce tour.
+	 */
+	private int getManaTour() {
+		int mana;
+		if (this.tour<10) mana=this.tour;
+		else mana=10;
+		return mana;
+	}
+
+	/**
+	 * organise la manière de piocher à chaque tour
+	 * @param joueur qui pioche
+	 */
+	private void piocher(Joueur joueur) {
+		if ((this.tour==0)&&(joueur==this.j1)){
+			joueur.piocher();
+			joueur.piocher();
+			joueur.piocher();
+		}
+		else if ((this.tour==0)&&(joueur==this.j2)){
+			joueur.piocher();
+			joueur.piocher();
+			joueur.piocher();
+			joueur.piocher();
+		}
+		else {
+			joueur.piocher();
+		}
 		
 	}
 }
