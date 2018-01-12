430224a39b1b32e78e8f9681cd85e13ece06b6e0
crowlas <dracosamu@gmail.com>
2018-01-12 22:10:50 +0100

    sort benediction puissance
    non finie

--- HearthStone/src/carte/CartePalaBenedictionDePuissance.java
+++ HearthStone/src/carte/CartePalaBenedictionDePuissance.java
@@ -1,11 +1,21 @@
 package carte;
 
+import plateau.Plateau;
+
 public class CartePalaBenedictionDePuissance extends Sort {
 
 	public CartePalaBenedictionDePuissance() {
 		this.nom = "Benediction de puissance";
 		this.coutMana = 1;
 		this.description = "Confere plus 3 attaque a un serviteur";
+	}
+
+	@Override
+	public void activer(boolean isJ1, Plateau plateau) {
+		int indexServiteur=0;
+		// TODO choix serviteur
+		plateau.boostAttaque(3, indexServiteur, isJ1);
+		
 	}
 	
 }
