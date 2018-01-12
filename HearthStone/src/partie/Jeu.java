ebc217b00f3f87234084a11db53675676897eb42
crowlas <dracosamu@gmail.com>
2018-01-12 09:58:25 +0100

    organisation d'un tour de jeu

--- HearthStone/src/partie/Jeu.java
+++ HearthStone/src/partie/Jeu.java
@@ -8,4 +8,36 @@ public class Jeu {
 	private Joueur j1;
 	private Joueur j2;
 	
+	
+	public Jeu(Joueur j1, Joueur j2) {
+		this.j1 = j1;
+		this.j2 = j2;
+		this.tour = 0;
+		this.plateau=new Plateau();
+	}
+
+	/**
+	 * méthode lancé pour le tour d'un joueur
+	 * elle est lancé une fois par joueur à chaque tour
+	 */
+	public void tourDeJeu(int tour){
+			int mana = getManaTour(tour);
+			Joueur joueurPerdant = this.j1.jouer(mana);
+			if (joueurPerdant!=null) this.finDuJeu(joueurPerdant);
+			joueurPerdant = this.j2.jouer(mana);
+			if (joueurPerdant!=null) this.finDuJeu(joueurPerdant);
+			
+		}
+		
+	}
+
+	private int getManaTour(int tour2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private void tour0() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- HearthStone/src/partie/Joueur.java
+++ HearthStone/src/partie/Joueur.java
@@ -11,5 +11,20 @@ public class Joueur {
 	
 	public void piocher(){
 		// TODO
+	}
+	
+	/**
+	 * Appelé par Jeu afin de permettre au joueur de jouer.
+	 * @param mana maximal du joueur pour ce tour
+	 * @return le joueur perdant. Null si il n'y en a pas.
+	 */
+	public Joueur jouer(int mana) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean aPerdu() {
+		return this.heros.estMort();
 	};
 }
