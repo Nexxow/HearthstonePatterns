db5b91d2f953cc053eab798b333db2f6108efb25
crowlas <dracosamu@gmail.com>
2018-01-12 11:02:12 +0100

    Joueur près à être implémenter

--- HearthStone/src/partie/Joueur.java
+++ HearthStone/src/partie/Joueur.java
@@ -1,27 +1,38 @@
 package partie;
 
+import java.util.ArrayList;
+
 import pioche.Pioche;
 import heros.Heros;
 import carte.Carte;
 
 public class Joueur {
-	private Carte[] main;
+	private ArrayList<Carte> main;
 	private Heros heros;
 	private Pioche pioche;
 	
+	/**
+	 * Constructeur de classe.
+	 * Les attributs de la classe sont initialisés durant une partie
+	 */
+	public Joueur() {
+		super();
+	}
+	
+	
 	public void piocher(){
-		// TODO
+		//this.main.add(this.pioche.fabriquerCarte("")); // en attente d'une création aléatoire de carte
 	}
 	
 	/**
 	 * Appelé par Jeu afin de permettre au joueur de jouer.
+	 * 
 	 * @param mana maximal du joueur pour ce tour
 	 * @return le joueur perdant. Null si il n'y en a pas.
 	 */
 	public Joueur jouer(int mana) {
-		return null;
-		// TODO Auto-generated method stub
-		
+		//TODO demande au joueur de selectionner des cartes et des actions.
+		return null;		
 	}
 
 	public boolean aPerdu() {
